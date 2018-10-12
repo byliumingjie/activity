ff2b269dce8fe3c6ef7e8cb73f6219c31c12be67
byliumingjie <liumingjie@gank-studio.com>
2018-10-12 11:17:31 +0800

    by liumj add

--- index.php
+++ index.php
@@ -8,15 +8,14 @@ $act_type = "ranking"; // $act_type = "lottery"; // signin
 $json_config_name = $apppid . '.json';
 
 $util = new Utils();
-    $act_mod = new  ActivityModel();
+$act_mod = new  ActivityModel();
 
 $data = $util->getFileConfig($json_config_name);
 $act_rule = json_decode($data['act_rule'], true);
-//var_dump($act_rule);
 
 switch ($act_type) {
     case 'ranking':
-        echo $act_mod->rankingBack();
+        $redvar = $act_mod->rankingBack();
         break;
     case 'lottery':
         break;
